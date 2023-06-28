5e1206a09bd04a3695fb538dbf80c909f966beb7
lilieth chacon <liliethramirez91@gmail.com>
2023-06-28 18:55:28 -0300

    correccion css movile y js

--- js/main.js
+++ js/main.js
@@ -1,29 +1,14 @@
-// SELECCIONAR TODOS LOS ITEMS DEL MENU
-const menuItems = document.querySelectorAll('.menu li a');
 
-// Recorro la lista de nodos y detectar el click en todos ellos
+const menuItems = document.querySelectorAll('.menu-item');// selecciono los item 
+
 menuItems.forEach(function(item) {
-  item.addEventListener('click', function(event) {
-    event.preventDefault();
-
-    // Quitar la clase "active" de todos los elementos del menú
+  item.addEventListener('click', function(e) {
+    // Elimino la clase 'active' de todos los elementos del menú
     menuItems.forEach(function(item) {
       item.classList.remove('active');
     });
-
-    // Agregar la clase "active" al elemento que fue clickeado
-    this.classList.add('active');
-
-    // Obtener el ID del objetivo del enlace
-    const targetId = this.getAttribute('href').substring(1);
-
-    // Obtener el elemento objetivo por su ID
-    const target = document.getElementById(targetId);
-
-    // aca un Desplazamiento suave hacia el elemento objetivo
-    target.scrollIntoView({ behavior: 'smooth' });
+    
+    // agrego la clase 'active' al elemento que fue clickeado
+    e.target.classList.add('active');
   });
 });
-
-// clase "active" al elemento "Inicio" por defecto
-menuItems[0].classList.add('active');
